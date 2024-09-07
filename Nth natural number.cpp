8cacab219627e9574ebafbab976ba40184783270
Disha Mukhopadhyay <113844180+Disha-16@users.noreply.github.com>
2024-09-07 12:58:58 +0530

    Create Nth natural number.cpp

---
+++ Nth natural number.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long findNth(long long n) {
+        // code here.
+        long long result=0;
+        long long mult=1;
+        while(n){
+            result+= (mult)*(n%9);
+            mult= mult*10;
+            n=n/9;
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, ans;
+        cin >> n;
+        Solution obj;
+        ans = obj.findNth(n);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
